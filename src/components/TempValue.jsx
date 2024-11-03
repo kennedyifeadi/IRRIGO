46bdf5b080b46d6cc87f00d5cabe44d9921756d8
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-03 23:50:15 +0100

    Update TempValue component with temperature display

--- src/components/TempValue.jsx
+++ src/components/TempValue.jsx
@@ -1,6 +1,10 @@
-
+import { FaTemperatureLow } from "react-icons/fa";
 export const TempValue = () => {
   return (
-    <div>TempValue</div>
+    <div className="h-[90px] rounded-md w-[30%] bg-[#64B5F6] p-2 text-[#333333] flex flex-col items-center">
+      <span className="text-[22px]"><FaTemperatureLow /> </span>
+      <h1 className="text-[24px] font-bold text-[white]">40%</h1>
+      <span className="text-[10px]">Temp level</span>
+      </div>
   )
 }
