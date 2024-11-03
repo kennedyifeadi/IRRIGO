f53e6297123b98eb85a6f3f924fd99b34fef91de
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-03 23:43:58 +0100

    Update HumidValue component and Dashboard page

--- src/components/HumidValue.jsx
+++ src/components/HumidValue.jsx
@@ -1,7 +1,11 @@
-
+import { WiHumidity } from "react-icons/wi";
 export const HumidValue = () => {
   return (
-    <div>HumidValue</div>
+    <div className="h-[90px] rounded-md w-[30%] bg-[#F9A825] p-2 text-[#333333] flex flex-col items-center">
+      <span className="text-[22px]"><WiHumidity /> </span>
+      <h1 className="text-[24px] font-bold text-[white]">80%</h1>
+      <span className="text-[10px]">Humidity level</span>
+      </div>
   )
 }
 
