53f311117fd47bdb0eeae2230149089e301a6efb
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-04 00:39:07 +0100

    Update BottomNav component styling and background color Update Cycle component with cycle display and restart button

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -5,7 +5,7 @@ import { BsFillBarChartLineFill } from "react-icons/bs";
 export const BottomNav = () => {
   return (
     <div className="fixed bottom-3 w-full h-[60px] flex justify-center">
-      <div className="flex justify-between h-full shadow-[0px_0px_5px_#0000004d] w-[90%] rounded-full ">
+      <div className="flex justify-between h-full shadow-[0px_0px_5px_#0000004d] bg-white w-[90%] rounded-full ">
         <ul className="flex justify-between items-center px-5 h-full w-full  ">
           <li className="text-[#4CAF50] text-[24px] flex flex-col justify-center items-center"><IoHomeSharp /> <span className="text-[14px]">Home</span></li>
           <li className="text-[#333333] text-[24px] flex flex-col justify-center items-center "><BsFillBarChartLineFill /> <span className="text-[14px]">Analytics</span></li>
--- src/components/Cycle.jsx
+++ src/components/Cycle.jsx
@@ -1,6 +1,20 @@
-
+import { GiCycle } from "react-icons/gi";
 export const Cycle = () => {
   return (
-    <div>Cycle</div>
+    <div className="w-[90%] rounded-md flex justify-between shadow-md py-2 px-4 bg-white mt-5">
+      <div className="flex flex-col " >
+      <h1 className="font-bold text-[24px] text-[#333333]">Cycle</h1>
+        <div className="flex gap-1 items-center">
+          <span className="text-[#333333] text-[36px]"><GiCycle /></span>
+          <span className="flex flex-col">
+            <span className="text-[18px]">Next Cycle</span>
+            <span className="text-[10px] text-[#4CAF50] font-semibold">•15mins•</span>
+          </span>
+        </div>
+      </div>
+      <div className="justify-center items-center flex ">
+          <button className="bg-[#64B5F6] rounded-lg active:scale-90 duration-200 ease-in-out px-4 text-white py-2">Restart</button>
+      </div>
+    </div>
   )
 }
