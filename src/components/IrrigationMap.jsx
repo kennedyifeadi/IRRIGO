960e1f3718a03177ea57c3e3fbc98f551c6d6175
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-04 01:34:07 +0100

    Update IrrigationMap component with grid layout and hover effects

--- src/components/IrrigationMap.jsx
+++ src/components/IrrigationMap.jsx
@@ -1,6 +1,20 @@
 
 export const IrrigationMap = () => {
   return (
-    <div className="mt-5 w-[90%]">IrrigationMap</div>
+    <div className="mt-5 w-[90%] h-[200px] border-2 border-dashed p-3 rounded-lg border-[#8D6E63] grid grid-cols-4 gap-3">
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+      <div className="border-[#8D6E63] border-dashed rounded-md border-2 hover:bg-[gray] hover:border-[#F4F4F9] hover:border-solid duration-1000 ease-in-out origin-center"></div>
+
+    </div>
   )
 }
