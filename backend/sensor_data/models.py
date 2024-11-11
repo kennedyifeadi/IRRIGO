7603d47416cf48c2b1fc15be1342173cc87f1d8b
Matthew-123-dev <olagunjunifemi6@gmail.com>
2024-11-11 10:27:37 +0100

    Add Location model and update __str__ methods in SensorData and Location models

--- backend/sensor_data/models.py
+++ backend/sensor_data/models.py
@@ -6,6 +6,15 @@ class SensorData(models.Model):
     humidity = models.FloatField()
     soil_moisture = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f'Temperature: {self.temperature}, Humidity: {self.humidity}%, Soil Moisture: {self.soil_moisture}% , Created At: {self.created_at}'
+    
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    latitude= models.DecimalField(max_digits= 10, decimal_places= 6)
+    longitude = models.DecimalField(max_digits=1, decimal_places= 6)
+    updated_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return f'{self.name} ({self.latitude}, {self.longitude})'if self.name else f'Location, ({self.latitude}, {self.longitude})'
+    
