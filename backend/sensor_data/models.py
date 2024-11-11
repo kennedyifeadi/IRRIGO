fa3f3e0a82d5d7e69c83a85ac7fab1901e5f1141
Matthew-123-dev <olagunjunifemi6@gmail.com>
2024-11-11 13:54:59 +0100

    Add Location model and register it in admin; create migration for HardWaredata and Location models

--- backend/sensor_data/models.py
+++ backend/sensor_data/models.py
@@ -12,7 +12,7 @@ class SensorData(models.Model):
 class Location(models.Model):
     name = models.CharField(max_length=50)
     latitude= models.DecimalField(max_digits= 10, decimal_places= 6)
-    longitude = models.DecimalField(max_digits=1, decimal_places= 6)
+    longitude = models.DecimalField(max_digits=10, decimal_places= 6)
     updated_at = models.DateTimeField(auto_now= True)
 
     def __str__(self):
