f3b266792786d6e41a00ced1029a10a6f8ff63b0
Matthew-123-dev <olagunjunifemi6@gmail.com>
2024-11-11 14:50:06 +0100

    Remove HardWaredata model; add battery_level and water_level fields to SensorData model

--- backend/sensor_data/models.py
+++ backend/sensor_data/models.py
@@ -6,8 +6,10 @@ class SensorData(models.Model):
     humidity = models.FloatField()
     soil_moisture = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
+    battery_level = models.FloatField()
+    water_level = models.FloatField()
     def __str__(self):
-        return f'Temperature: {self.temperature}, Humidity: {self.humidity}%, Soil Moisture: {self.soil_moisture}% , Created At: {self.created_at}'
+        return f'Temperature: {self.temperature}, Humidity: {self.humidity}%, Soil Moisture: {self.soil_moisture}% , Created At: {self.created_at}, , Battery level: {self.battery_level}, Water level: {self.water_level}'
     
 class Location(models.Model):
     name = models.CharField(max_length=50)
@@ -18,10 +20,3 @@ class Location(models.Model):
     def __str__(self):
         return f'{self.name} ({self.latitude}, {self.longitude})'if self.name else f'Location, ({self.latitude}, {self.longitude})'
 
-class HardWaredata(models.Model):
-    battery_level = models.FloatField()
-    water_level = models.FloatField()
-
-    def __str__(self):
-        return f'Battery level: {self.battery_level}, Water level: {self.water_level}'
-       
