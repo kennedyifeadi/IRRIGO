78c163b20b81e433b67ac2ea6d1d022890ca9436
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-13 06:34:37 +0100

    Update humidity, moisture, and temperature value components

--- src/components/MoistValue.jsx
+++ src/components/MoistValue.jsx
@@ -4,7 +4,7 @@ export const MoistValue = () => {
     <div className="h-[90px] rounded-md w-[30%] bg-[#8D6E63] p-2 text-[#333333] flex flex-col items-center">
       <span className="text-[22px]"><BsMoisture /> </span>
       <h1 className="text-[24px] font-bold text-[white]">65%</h1>
-      <span className="text-[10px]">Moisture level</span>
+      <span className="text-[10px] font-bold">Moisture level</span>
       </div>
   )
 }
--- src/components/TempValue.jsx
+++ src/components/TempValue.jsx
@@ -4,7 +4,7 @@ export const TempValue = () => {
     <div className="h-[90px] rounded-md w-[30%] bg-[#64B5F6] p-2 text-[#333333] flex flex-col items-center">
       <span className="text-[22px]"><FaTemperatureLow /> </span>
       <h1 className="text-[24px] font-bold text-[white]">40%</h1>
-      <span className="text-[10px]">Temp level</span>
+      <span className="text-[10px] font-bold">Temp level</span>
       </div>
   )
 }
