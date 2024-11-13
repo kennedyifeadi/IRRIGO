8cc2bb473f9fa42a5f2f2ef920cf1349c379ed5f
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-13 12:04:38 +0100

    Refactor AiInference component to improve layout and add content

--- src/components/AiInference.jsx
+++ src/components/AiInference.jsx
@@ -1,7 +1,7 @@
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 export const AiInference = () => {
   return (
-    <div className="mt-5 w-[95%] flex flex-col rounded-lg px-4  bg-white h-[300px]">
+    <div className="mt-5 w-[95%] flex flex-col rounded-lg px-4 pb-4 items-center bg-white h-[300px]">
       <div className="flex justify-between w-full h-[20%] items-center">
         <h1 className='font-semibold'>AI Summary</h1>
         <DotLottieReact
@@ -11,7 +11,9 @@ export const AiInference = () => {
       className='w-24 h-24 mr-[-30px]'
     />
       </div>
-      <div className="flax w-full overflow-y-auto h-[80%]"></div>
+      <div className="flax w-[99.5%] p-4 rounded-md bg-[#80808059] overflow-y-auto h-[80%]">
+        <span className='w-full h-max max-h-full overflow-y-auto border border-black'>frgewgeqgewgewgewrgr</span>
+      </div>
     </div>
   )
 }
